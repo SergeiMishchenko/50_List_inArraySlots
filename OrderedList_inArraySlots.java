a10c75b90edf7273f4d7139d9861655683069129
SergeiMishchenko <sergei.mish26@gmail.com>
2019-04-01 12:23:52 -0400

    hw5A_16

--- OrderedList_inArraySlots.java
+++ OrderedList_inArraySlots.java
@@ -8,15 +8,13 @@
  */
 
 public class OrderedList_inArraySlots {
-    private list List_inArraySlots;
 
-    private static final int INITIAL_CAPACITY = 10;
-
+    private List_inArraySlots list;
     /**
       Construct an empty list with a small initial capacity.
      */
     public OrderedList_inArraySlots() {
-        list = new List_inArraySlots;
+        list = new List_inArraySlots();
         // filledElements has been initialized to the desired value, 0
     }
 
@@ -25,7 +23,7 @@ public class OrderedList_inArraySlots {
       @return the number of elements in this list
      */
     public int size() {
-        return list.filledElements;
+        return list.size();
     }
 
 
@@ -34,10 +32,7 @@ public class OrderedList_inArraySlots {
        in [a,b,c,] format
       */
     public String toString() {
-        String result = "[";
-        for( int elemIndex = 0; elemIndex < list.filledElements; elemIndex++)
-            result += list.elements[ elemIndex] + ",";
-        return result + "]";
+        return list.toString();
     }
 
 
@@ -47,17 +42,17 @@ public class OrderedList_inArraySlots {
       @return true, in keeping with conventions yet to be discussed
      */
      public boolean add( int value) {
-        return list.add (value);
+         // expand if necessary
+         return list.add(value);
 }
 
 
     /**
-      Double the capacity of the List_inArraySlots,
+      expand method not necessary in ordered list. Assumes it will be implemented in List_inArraySlots
+	  Double the capacity of the List_inArraySlots,
       preserving existing data.
      */
-     private void expand() {
-        list.expand();
-     }
+
 
     // --------- end of "code that worked in v0" ---------
 
@@ -71,21 +66,22 @@ public class OrderedList_inArraySlots {
            whether user violated the condition.)
      */
     public int get( int index ) {
-        return list.get(index)
+        // E-Z! pass through the request to the array object
+        return list.get(index);
     }
 
 
     /**
-      No set method.   
+      o set method 
 
-      Set value at @index to @newValue
-
-      @return old value at @index
-      @precondition: @index is within the bounds of this list.
      */
-      
+ /*    public int set( int index, int newValue ) {
+        int saveForReturn = get( index);
+        elements[ index] = newValue;
+        return saveForReturn;
+    } */
 
-      No add method to an index of a value
+
     /**
       Insert @value at position @index in this list.
 
@@ -95,24 +91,27 @@ public class OrderedList_inArraySlots {
      */
 
      //not a valid method in OrderedList_inArraySlots
-     public void add( int index, int value) {
-        if( index == filledElements) // adding at end of list
-            add( value);
-        else {// need space
-             // open up space, expanding if necessary
-             add( elements[ filledElements-1]);
 
-             // move the hole left / shift "subsequent elements" right
-             for( int hole = filledElements-1; hole > index; hole--)
-                 elements[ hole] = elements[ hole-1];  
+     // public void add( int index, int value) {
+        // if( index == filledElements) // adding at end of list
+            // add( value);
+        // else {// need space
+             // // open up space, expanding if necessary
+             // add( elements[ filledElements-1]);
 
-            elements[ index] = value; // store new value
-        }
-     }
-     public void addAtHead( int value) {
-        add (0, value); //adds at beginning of the list
+             // // move the hole left / shift "subsequent elements" right
+             // for( int hole = filledElements-1; hole > index; hole--)
+                 // elements[ hole] = elements[ hole-1];
+
+            // elements[ index] = value; // store new value
+        // }
+     // }
+	 
+	 //no addAtHead method
+     // public void addAtHead( int value) {
+        // add (0, value); //adds at beginning of the list
         
-     }
+     // }
      /**
       Remove the element at position @index in this list.
 
